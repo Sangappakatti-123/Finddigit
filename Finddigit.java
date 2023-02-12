9188055d0c9bed449f7a87abc2a676e87a91d557
Sangappakatti-123 <116739074+Sangappakatti-123@users.noreply.github.com>
2023-02-12 10:29:41 +0530

    Create Finddigit.java

---
+++ Finddigit.java
@@ -0,0 +1,42 @@
+ public static int findDigits(int n) {
+    // Write your code here
+      int temp=n;
+      int a=0;
+      while(temp>0){
+          int rem=temp%10;
+          if(rem!=0){
+          if(n%rem==0){
+              a++;
+          }
+          }
+          temp=temp/10;
+      }
+      return a;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int result = Result.findDigits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
